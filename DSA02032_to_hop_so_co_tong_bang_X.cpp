31d6205305d3b1d9881f985d7458690aab197b77
Chu Mai <chuthimai18245@gmail.com>
2023-02-26 09:22:56 +0700

    to_hop_so_co_tong_bang_X
    sử dụng đệ quy
    
    - đặt sum=0
    - nếu sum>X thì quay lại
    - nếu sum<X thì đệ quy tiếp
    - nếu sum==X thì in ra

--- DSA02032_to_hop_so_co_tong_bang_X.cpp
+++ DSA02032_to_hop_so_co_tong_bang_X.cpp
@@ -1,56 +1,77 @@
-////
-////  DSA02032_to_hop_so_co_tong_bang_X.cpp
-////  CodePTIT_cau_truc_du_lieu_va_giai_thuat
-////
-////  Created by Chu Mai on 23/02/2023.
-////
 //
-//#include <bits/stdc++.h>
-//using namespace std;
+//  DSA02032_to_hop_so_co_tong_bang_X.cpp
+//  CodePTIT_cau_truc_du_lieu_va_giai_thuat
 //
-//int A[35], C[35];
-//int n, X, s;
-//int OK = 0;
-//vector<vector<int>> save;
+//  Created by Chu Mai on 23/02/2023.
 //
-//
-//void in(int i){
-//    printf("{");
-//    for (int j=0; j<=i; j++) {
-//        if(j!=i) printf("%d ", C[j]);
-//        else printf("%d", C[j]);
-//        
-//    }
-//    printf("} ");
-//}
-//
-//void quay_lui(int i, int s){
-//    int j;
-//    for(j=0; j<n; j++){
-//        if(A[j]>=C[i-1] && s+A[j]<=X){
-//            C[i]=A[j];
-//            s = s+A[j];
-//            if(s==X){
-//                OK ++; in(i);
-//            }else quay_lui(i+1, s);
-//            s = s-A[j];
-//        }
-//    }
-//}
-//
-//void oneTime(){
-//    cin>>n>>X;
-//    for(int i=0; i<n; i++) cin>>A[i];
-//    quay_lui(0, 0);
-//    if(OK == 0) printf("-1");
-//    cout<<endl;
-//}
-//
-//int main(){
-//    int n;
-//    cin>>n;
-//    for(int i=0; i<n; i++) oneTime();
-//}
-//
-//
-//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, X;
+vector<int> A, C;
+vector<vector<int>> save;
+int OK;
+
+
+void in(vector<int> arr){
+    long long n = arr.size();
+    printf("{");
+    for(int i=0; i<n; i++){
+        if(i==n-1) printf("%d", arr[i]);
+        else printf("%d ", arr[i]);
+    }
+    printf("} ");
+}
+
+void to_hop(int i, int sum){
+    for(int j=0; j<n; j++){
+        
+        int t;
+        if(i==0) t=0;
+        else t=C[i-1];
+        
+        if(A[j]>=t and sum+A[j]<=X){
+            C.push_back(A[j]);
+            sum += A[j];
+            if(sum == X){
+                OK++;
+                save.push_back(C);
+            }else if(sum<X) to_hop(i+1, sum);
+            C.pop_back();
+            sum -= A[j];
+        }
+    }
+}
+
+void oneTime(){
+    A.clear();
+    C.clear();
+    save.clear();
+    OK = 0;
+    
+    cin>>n>>X;
+    for(int i=0; i<n; i++){
+        int a; cin>>a;
+        A.push_back(a);
+    }
+    
+    to_hop(0, 0);
+    if(OK==0) printf("-1");
+    else{
+        printf("%d ", OK);
+        for(int i=0; i<OK; i++){
+            in(save[i]);
+        }
+    }
+    cout<<endl;
+    
+}
+
+
+int main(){
+    int n; cin>>n;
+    for(int i=0; i<n; i++) oneTime();
+}
+
+
