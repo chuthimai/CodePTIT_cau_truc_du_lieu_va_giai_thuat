d35e3cf1a99ad181dae8eec61bdc15c3b7aa16de
Chu Mai <chuthimai18245@gmail.com>
2023-04-06 13:15:15 +0700

    DSA08019_so_loc_phat_3

---
+++ DSA08019_so_loc_phat_3.cpp
@@ -0,0 +1,43 @@
+//
+//  DSA08019_so_loc_phat_3.cpp
+//  CodePTIT_cau_truc_du_lieu_va_giai_thuat
+//
+//  Created by Chu Mai on 06/04/2023.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void oneTime(){
+    int n;
+    cin>>n;
+    queue<string> Q;
+    stack<string> res;
+    Q.push("6");
+    Q.push("8");
+    string s=Q.front();
+    while (s.length()<=n) {
+        res.push(s);
+        Q.push(s+"6");
+        Q.push(s+"8");
+        Q.pop();
+        s = Q.front();
+    }
+    cout<<res.size()<<endl;
+    while(!res.empty()){
+        cout<<res.top()<<" ";
+        res.pop();
+    }
+    cout<<endl;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++) oneTime();
+}
+
+
+
+
